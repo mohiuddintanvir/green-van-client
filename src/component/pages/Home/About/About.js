1771790a2ebe160ccea4cd0b1ee336feb57ae8b0
mohiuddintanvir <mohiuddin.tanvir.mollah@gmail.com>
2022-11-10 19:53:12 +0600

    forteen

--- src/component/pages/Home/About/About.js
+++ src/component/pages/Home/About/About.js
@@ -1,19 +1,20 @@
 import React from 'react';
+import './about.css'
 
 const About = () => {
     return (
-        <div className="hero  bg-base-200">
+        <div className="hero  bg-base-200 bg p-20">
             <div className="hero-content flex-col lg:flex-row">
                 <img className='w-80' src="https://i0.wp.com/orissadiary.com/wp-content/uploads/2022/04/Get-Back-To-Nature-1.jpeg?resize=450%2C300&ssl=1" alt="" />
                 <div>
-                    <h1 className="text-5xl ">This is online based sole-propartise business.</h1>
+                    <h1 className="text-5xl  ">This is online based sole-propartise business.</h1>
                     <p className="py-6">The products you see  this website  are fully garanted organic food.I am the person who grow this vagetable and sell here .
 
                         First superior full-bodied drink. Like outstanding odor economical deal clinically
                         Organic farming is the production of food without the use of synthetic chemicals or genetically modified components.
                         Organic foods are not necessarily completely chemical free,
                         but the pesticide residues will be considerably lower than those found in produce manufactured with synthetic chemicals.</p>
-                    <button className="btn btn-primary">Get Started</button>
+
                 </div>
             </div>
         </div>
